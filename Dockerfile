bec3e402e1a1722a6a3c7d7a4ceb754dd986d80c
liudaojie <liudaojie@yy.com>
2020-03-08 01:39:01 +0800

    feat: first commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM jackness1208/yyd-base:latest
+RUN npm i yyd@0.3.1 -g
---
+++ yyd.config.js
@@ -0,0 +1,22 @@
+const pkg = require('./package.json');
+module.exports = {
+  // docker 名称
+  repository: 'yyd',
+  // tag
+  tag: pkg.version,
+  // 推送的
+  pushHost: '',
+  pushPrefix: 'jackness1208',
+  // 自动 更新 history
+  rewriteHistory: true
+
+  // port 映射
+  // portMap: {
+  //   80:80
+  // },
+
+  // 挂载设置 [本地目录:docker 目录]
+  // volume: {
+  //   './autorun': '/autorun'
+  // },
+};
